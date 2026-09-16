67ad2955cd331f7c03a95423d7dbb4696c5952fe
Nemanja Vujanic <vujanic_nemanja@hotmail.com>
2026-09-16 11:32:06 +0200

    Fix dynamic Solutions sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,5 @@
 import type { MetadataRoute } from 'next'
+import { connection } from 'next/server'
 import { findAllSolutions } from '@/lib/content-data'
 
 const siteUrl = 'https://devstudio.biz'
@@ -9,6 +10,7 @@ const staticPaths = [
 ]
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  await connection()
   const [bhsSolutions, englishSolutions] = await Promise.all([
     findAllSolutions('bhs'),
     findAllSolutions('en'),
